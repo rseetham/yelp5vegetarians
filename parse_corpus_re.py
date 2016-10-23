156fc8f37b882ac193e78bd103fa86b809008677
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-23 12:15:33 -0500

    parse corpus re sort word count

--- parse_corpus_re.py
+++ parse_corpus_re.py
@@ -11,6 +11,9 @@ with open(os.path.join(cwd, 'corpus_re.txt'), 'r') as f:
 f.close()
 wordcount_list.sort(reverse=True, key=lambda x: x[1])
 output_wc = open(os.path.join(cwd, 'sorted_wc.txt'), 'w')
+output_wc2 = open(os.path.join(cwd, 'sorted_wc2.txt'), 'w')
 for i in wordcount_list:
-    output_wc.write(str(i)+'\n')
+    output_wc.write(str(i) + '\n')
+    output_wc2.write(i[0]+','+str(i[1])+'\n')
 output_wc.close()
+output_wc2.close()
