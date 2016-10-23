0c2c59a5215f3dc01bee6075391bed85434e8a4a
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-22 22:52:54 -0500

    write review text to file

--- json_parser_script_scratch.py
+++ json_parser_script_scratch.py
@@ -2,7 +2,7 @@ import json, os
 
 cwd = os.getcwd()
 json_review_list = []
-with open(os.path.join(cwd,'yelp_training_set','yelp_dataset_challenge_academic_dataset')) as f:
+with open(os.path.join(cwd,'yelp_training_set','yelp_dataset_challenge_academic_dataset.json')) as f:
     jfile={}
     for line in f:
         while True:
@@ -15,7 +15,7 @@ with open(os.path.join(cwd,'yelp_training_set','yelp_dataset_challenge_academic_
         # do something with jfile
         json_review_list.append(jfile)
 f.close()
-with open(os.path.join(cwd, 'yelp_training_set', 'corpus'),'w') as f:
+with open(os.path.join(cwd, 'corpus'),'w') as f:
     for i in range(0,len(json_review_list)):
         if json_review_list[i]['type'] == 'review':
             text = json_review_list[i]['text']
