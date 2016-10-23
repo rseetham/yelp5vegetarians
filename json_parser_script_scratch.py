f2682c13a75095875daebb24ae353becc9803522
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-22 22:47:24 -0500

    write review text to file

--- json_parser_script_scratch.py
+++ json_parser_script_scratch.py
@@ -2,7 +2,7 @@ import json, os
 
 cwd = os.getcwd()
 json_review_list = []
-with open(os.path.join(cwd,'yelp_training_set','yelp_training_set_review.json')) as f:
+with open(os.path.join(cwd,'yelp_training_set','yelp_dataset_challenge_academic_dataset')) as f:
     jfile={}
     for line in f:
         while True:
