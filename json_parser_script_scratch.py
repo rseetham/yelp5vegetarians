e67441b0014b5e7d7de050b45351156a60255016
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-22 21:59:11 -0500

    Update json_parser_script_scratch.py

--- json_parser_script_scratch.py
+++ json_parser_script_scratch.py
@@ -14,6 +14,12 @@ with open(os.path.join(cwd,'yelp_training_set','yelp_training_set_review.json'))
                 line += next(f)
         # do something with jfile
         json_review_list.append(jfile)
+with open(os.path.join(cwd,'corpus'),'w') as f:
+    for i in range(0,len(json_review_list)):
+        
+        text = json_review_list[i]['text']
+        if text:
+            f.write(text)
 print(len(json_review_list))
 print(json_review_list[0])
 print(type(json_review_list[0]))
