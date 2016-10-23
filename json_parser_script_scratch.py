26e2e52c7982587620e415dfb3136de4f2fc7bb4
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-23 02:55:26 -0500

    ignore weird encodings, like japanese katakana, accents, chinese characters, bullet points, etc

--- json_parser_script_scratch.py
+++ json_parser_script_scratch.py
@@ -8,11 +8,11 @@ with open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'categori
     reader = csv.reader(f)
     categories_list = list(reader)
 f.close()
-
+businessIDcsv = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'businessID.csv'),'w')
 with open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'yelp_academic_dataset_business.json')) as f:
     jfile = {}
     for line in f:
-        if line:
+        while True:
             try:
                 jfile = json.loads(line)
                 break
@@ -22,18 +22,17 @@ with open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'yelp_aca
         # do something with jfile
         if jfile:
             categories_set = {elem for elem in jfile['categories']}
-            # print(categories_set)
             categories_list_set = {elem for elem in categories_list[0]}
-            # print(categories_list_set)
-            if not categories_set.isdisjoint(categories_list_set):
+            if not categories_set.isdisjoint(categories_list_set) and (jfile['business_id'] not in json_businessid_list):
                 json_businessid_list.append(jfile['business_id'])
+                businessIDcsv.write(jfile['business_id']+'\n')
 f.close()
-
+businessIDcsv.close()
 
 with open(os.path.join(cwd,'yelp_dataset_challenge_academic_dataset', 'yelp_academic_dataset_review.json')) as f:
     jfile={}
     for line in f:
-        if line:
+        while True:
             try:
                 jfile = json.loads(line)
                 break
@@ -48,16 +47,16 @@ f.close()
 
 print(len(json_review_list))
 
-with open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'corpus'),'w') as f:
-    with open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'truncated_UserId_list.csv'), 'w') as fw:
-        for i in range(0,len(json_review_list)):
-            if json_review_list[i]['type'] == 'review':
-                text = json_review_list[i]['text']
-                if text:
-                    try:
-                        f.write(text)
-                        fw.write(json_review_list[i]['user_id']+',')
-                    except:
-                        pass
-f.close()
-fw.close()
+corpus = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'corpus'),'w')
+useridcsv = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'truncated_UserId_list.csv'), 'w')
+for i in range(0,len(json_review_list)):
+    if json_review_list[i]['type'] == 'review':
+        text = json_review_list[i]['text']
+        if text:
+            try:
+                corpus.write(text)
+                useridcsv.write(json_review_list[i]['user_id']+',')
+            except:
+                pass
+corpus.close()
+useridcsv.close()
