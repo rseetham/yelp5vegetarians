3d53e539ca65a56d42decea40e1d7b9b662e0e8b
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-22 23:35:44 -0500

    ignore weird encodings, like japanese katakana, accents, chinese characters, bullet points, etc

--- json_parser_script_scratch.py
+++ json_parser_script_scratch.py
@@ -15,10 +15,19 @@ with open(os.path.join(cwd,'yelp_dataset_challenge_academic_dataset', 'yelp_acad
         # do something with jfile
         json_review_list.append(jfile)
 f.close()
-with open(os.path.join(cwd, 'corpus'),'w') as f:
+# print(len(json_review_list))
+# number_char = 0
+with open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'corpus'),'w') as f:
     for i in range(0,len(json_review_list)):
         if json_review_list[i]['type'] == 'review':
             text = json_review_list[i]['text']
             if text:
-                f.write(text+'\n')
+                try:
+                    f.write(text)
+                except:
+                    pass
+                    # number_char+=1
+                    # print(i)
+                    # print(text)
+# print(number_char)
 f.close()
