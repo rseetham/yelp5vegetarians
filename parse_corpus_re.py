25fe12acfb6e5e86a955ba8de71de46eb0303cef
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-23 12:04:51 -0500

    uploaded user id list

---
+++ parse_corpus_re.py
@@ -0,0 +1,16 @@
+import os
+
+cwd = os.getcwd()
+
+wordcount_list = []
+with open(os.path.join(cwd, 'corpus_re.txt'), 'r') as f:
+    for line in f:
+        addition = line.split()
+        addition[1] = int(addition[1])
+        wordcount_list.append(addition)
+f.close()
+wordcount_list.sort(reverse=True, key=lambda x: x[1])
+output_wc = open(os.path.join(cwd, 'sorted_wc.txt'), 'w')
+for i in wordcount_list:
+    output_wc.write(str(i))
+output_wc.close()
