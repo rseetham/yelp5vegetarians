30bcc04b6dc217d1483be4aa775cd3f3b37592d8
Rekha Seethamraju <rseetha1@hawk.iit.edu>
2016-10-23 15:26:35 -0500

    added the code for ignoring non alphanum and covert to lowercase

--- word_count.py
+++ word_count.py
@@ -14,6 +14,11 @@ ipfile = sc.textFile("/corpus")
 //ipfile.count()
 wordCounts = ipfile.flatMap(lambda line: filter(None, re.split("[, \-!?:.;/]+",line))).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
 
+
+#Converts all the non alpha numeric chars to space and changes all chars to lower case :
+#wordCounts_re = ipfile.flatMap(lambda line: re.sub('[^0-9a-zA-Z]+', ' ', line).lower().split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+#
+
 counts = wordCounts.collect()
 f = open("corpus_output.txt","w")
 for (word,c) in counts :
