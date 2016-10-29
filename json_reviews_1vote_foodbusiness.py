e9ef7e2d489997ff4328672587316719027cf2be
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-29 14:17:36 -0500

    parse corpus re sort word count

--- json_reviews_1vote_foodbusiness.py
+++ json_reviews_1vote_foodbusiness.py
@@ -63,8 +63,8 @@ newdataset.close()
 countratings = []
 newdataset2 = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'dataset_1usefulreview_review_refiltered.json'),'w')
 corpus = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'corpus_1useful_review'),'w')
-useridcsv = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'truncated_UserId_list2.csv'), 'w')
-ratingscsv = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset', 'ratings.csv'), 'w')
+useridcsv = open(os.path.join(cwd, 'truncated_UserId_list2.csv'), 'w')
+ratingscsv = open(os.path.join(cwd, 'ratings.csv'), 'w')
 count = 0
 othercount = 0
 for i in range(0,len(json_review_list)):
@@ -88,4 +88,5 @@ print(len(countratings))
 # print(countratings)
 corpus.close()
 useridcsv.close()
+ratingscsv.close()
 # print()
