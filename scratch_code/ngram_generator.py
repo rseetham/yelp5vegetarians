58fcfe6eec2807d6d064aa11787ae85a1da91b70
gkim8hawkiit <gkim8@hawk.iit.edu>
2016-10-29 17:17:34 -0500

    parse corpus re sort word count

--- scratch_code/ngram_generator.py
+++ scratch_code/ngram_generator.py
@@ -1,10 +1,11 @@
 import os, re
 
 cwd = os.getcwd()
-input_list = ['all', 'this', 'happened', 'more', 'or', 'less']
-b = []
+# input_list = ['all', 'this', 'happened', 'more', 'or', 'less']
+# b = []
 def find_ngrams():
     # cwd = os.getcwd()
+    b = []
     f = open(os.path.join(cwd, 'yelp_dataset_challenge_academic_dataset','corpus_1useful_review.txt'))
     lines = f.flatMap(lambda line: re.sub('[^.a-zA-Z]', line).lower().split("."))
     for i in range(0,len(list(zip(*[lines[i:] for i in range(2)])))):
